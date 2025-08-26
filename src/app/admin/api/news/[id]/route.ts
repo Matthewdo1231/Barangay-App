727f96fe1330da6c58c3bbe4372083f62ba34c7c
matthewdo1231 <matthewdoarda@gmail.com>
2025-08-26 18:31:26 +0800

    feat: fixed prisma client error

--- src/app/admin/api/news/[id]/route.ts
+++ src/app/admin/api/news/[id]/route.ts
@@ -1,5 +1,5 @@
 "use server";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { requireAdmin } from "@/lib/requireAdmin";
 
 const prisma = new PrismaClient();
