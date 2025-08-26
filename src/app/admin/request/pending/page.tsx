76cd4ebcb96f6a4b3e29ed0b31866541939cf168
matthewdo1231 <matthewdoarda@gmail.com>
2025-08-26 20:00:05 +0800

    attempt fixed 17

--- src/app/admin/request/pending/page.tsx
+++ src/app/admin/request/pending/page.tsx
@@ -1,6 +1,8 @@
 import { getPendingRequests } from "./action";
 import PendingRequestsClient from "./_component/PendingRequest";
 
+export const dynamic = "force-dynamic";
+
 export default async function PendingRequestsPage() {
   const initialData = await getPendingRequests();
 
